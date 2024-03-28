af797e3afac974e3cbef2f98192641545e477e30
Alice Lim <lexiestrelka@gmail.com>
2024-03-28 17:39:33 +0800

    Write "HotStuff-rs Block Data" section of the module-level doc for block_data.

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,6 @@
 //! Rust implementations of the data types defined in the
 //! [ParallelChain Protocol](https://github.com/parallelchain-io/parallelchain-protocol), including
 //! transactions, blocks, cryptographic primitives, and RPC requests and responses.
-//! 
-//! ## Organization
-//!
-//! The organization of this library into modules mirror the organization of the protocol specification
-//! into chapters. For example, transactions, which are specified in the "Blockchain" chapter of the
-//! [specification](https://github.com/parallelchain-io/parallelchain-protocol/blob/master/Blockchain.md#transaction),
-//! are defined in the blockchain module of this [library](blockchain::Transaction). The exception are the cryptographic
-//! primitives specified in the Blockchain chapter, which are here defined in its own module.
 
 pub mod cryptography;
 
